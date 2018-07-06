f5f40b808ba6902b471c7060114e9d8b68b9a4cf
Your Name <shalom.mat40@gmail.com>
2018-07-06 18:38:08 +0300

    save new changes

--- src/playground/redux-cash-Tracker.js
+++ src/playground/redux-cash-Tracker.js
@@ -1,4 +1,37 @@
 import {createStore, combineReducers} from 'redux'
+
+
+
+const expensesReducersDState =[]
+const expensesReducers=(state =expensesReducersDState, action)=>{
+    switch(action.type){
+        default:
+        return state
+    }
+
+};
+
+const filterReducersDState = {
+    text: '',
+    sortBy: 'date',
+    startDate:undefined,
+    endDate:undefined
+}
+const filterReducers = (state = filterReducersDState,action)=>{
+    switch(action.type){
+        default:
+        return state
+    }
+}
+
+const store =createStore(combineReducers({
+    expenses: expensesReducers,
+    filters: filterReducers
+
+}));
+console.log(store.getState())
+
+
 const demoState={
     expenses:[{
         id: 'jjkssd',
